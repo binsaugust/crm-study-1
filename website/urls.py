d18e5110e6462dd245f5c02ae578c91d90deeaaa
binsaugust <binsaugust@gmail.com>
2023-06-28 07:24:32 +0530

    revisionon28

--- website/urls.py
+++ website/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
 
     path('',views.home,name='home'),
     path('page1/',views.page1,name='page1'),
+    path('logout/',views.logout_user,name='logout_user'),
 ]
     
--- website/views.py
+++ website/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+
+
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html',{})
+    if request.method=='POST':
+        user_name = request.POST['username']
+        pass_word = request.POST['password']
+        user_check = authenticate(request,username=user_name,password=pass_word)
+        if user_check is not None:
+            login(request,user_check)
+            messages.success(request,"successfully loginned")
+            return redirect('home')
+        else:
+            messages.success(request,"Incorrect username or password")
+            return redirect('home')
+    else:
+        return render(request,'home.html',{})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,"successfully loged out")
+    return redirect('home')
+
 def page1(request):
     return render(request,'page1.html',{})
 
